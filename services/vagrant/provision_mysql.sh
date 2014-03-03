6358f2bbd28aadd6ab05d64626b52b6cb674fd31
Jack Cushman <jcushman@gmail.com>
2014-03-02 20:27:40 -0500

    Smarter mysql provisioning.

--- services/vagrant/provision_mysql.sh
+++ services/vagrant/provision_mysql.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# initialize the mysql directory and create the perma database
-# both commands will fail harmlessly if they've already been completed
+stop mysql
 
 # initialize mysql directory
-mysql_install_db --user=mysql --datadir=/mysql_data/ &&
-  start mysql;
+if [ ! -d /mysql_data/mysql ]; then
+  mysql_install_db --user=mysql --datadir=/mysql_data/
+  start mysql
   mysqladmin -u root password root
+else
+  start mysql
+fi
 
 # create perma db
-start mysql;
-  mysql -uroot -proot -e "create database perma character set utf8;" &&
-  mysql -uroot -proot -e "grant all on perma.* to perma@'localhost' identified by 'perma';"
+if [ ! -d /mysql_data/perma ]; then
+  mysql -uroot -proot -e "create database perma character set utf8; grant all on perma.* to perma@'localhost' identified by 'perma';"
+fi
